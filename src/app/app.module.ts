310ad2ac1fa8db77ae7f99794f27025ebad2c4c2
Kien Nguyen <hikiennguyen@gmail.com>
2018-06-05 15:28:00 +0700

    Import Angular Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule, BREAKPOINT } from '@angular/flex-layout';
+import { MaterialModule } from './material/material.module';
 
 import { AppComponent } from './app.component';
 
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
